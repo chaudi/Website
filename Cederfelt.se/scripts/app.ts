e1e17066d7348402152faa2c0fe8fcd97eb15020
chaudi <cederfelt@live.com>
2018-12-30 20:48:09 +0100

    Add project files.

---
+++ Cederfelt.se/scripts/app.ts
@@ -0,0 +1,9 @@
+﻿//Person is now string
+function Welcome(person: string) {
+    return "Hello, " + person;
+}
+
+function ClickMeButton() {
+    var user = "Mithun Pattankar";
+    document.getElementById("divMsg").innerHTML = Welcome(user);
+}
---
+++ Cederfelt.se/wwwroot/scripts/app.js
@@ -0,0 +1,9 @@
+//Person is now string
+function Welcome(person) {
+    return "Hello, " + person;
+}
+function ClickMeButton() {
+    var user = "Mithun Pattankar";
+    document.getElementById("divMsg").innerHTML = Welcome(user);
+}
+//# sourceMappingURL=app.js.map
